ad2fd259f34ec8302d9303af05ff29348e3c252f
Sam Anderson <samel.2710@gmail.com>
2017-11-19 22:34:14 +0000

    Splitting day appointments by table

--- src/js/init.js
+++ src/js/init.js
@@ -2,20 +2,24 @@ import Papa from 'papaparse';
 
 const fileInput = document.getElementById('csvFileInput');
 
-// Test if FileReader API supported
+// Test if FileReader API supported and attach listeners to file input
 if(!window.FileReader) {
     document.body.classList.add('fr-unsupported');
 } else {
     fileInput.addEventListener('change', () => handleFileUpload());
 }
 
-
-
+/* 
+ * Handle adding of a file
+ */
 const handleFileUpload = () => {
     const selectedFile = fileInput.files[0];
     parseCSV(selectedFile);
 };
 
+/* 
+ * Parse the input CSV
+ */
 const parseCSV = (fileObj) => {
     Papa.parse(fileObj, {
         header: true,
@@ -26,21 +30,62 @@ const parseCSV = (fileObj) => {
     });
 };
 
-const processData = (results, files) => {
+/* 
+ * Process the parsed results and split into days/tables etc.
+ */
+const processData = (results) => {
     const columns = results.meta.fields;
     const dailyAppointments = getAppointmentsByDay(results.data);
-    
-    console.log(dailyAppointments);
+
+    /* 
+     * If a day has appointments, split up the monolithic array 
+     * into smaller objects by table
+     * Note: this overwrites the original entry for appointments[day]
+     * into appointments[day][table]
+     */
+    Object.keys(dailyAppointments).forEach((day) => {
+        if(dailyAppointments[day].length > 0){
+            dailyAppointments[day] = splitByTable(dailyAppointments[day]);
+        } else {
+            console.log('No appointments for ', day);
+        }
+    });
 };
 
+/* 
+ * Split the monolothic set of appointments by unique location
+ */
+const splitByTable = (appointments) => {
+    const unique = [...new Set(appointments.map(apt => apt.meeting_point_name))];
+    let locations = {};
+
+    unique.forEach(location => {
+        locations[location] = appointments.filter(apt => apt.meeting_point_name === location);
+    });
+    return locations;
+}
+
+/* 
+ * Helper function to convert day index to human readable
+ */
+const dayOfWeekAsString = (dayIndex) => {
+    return ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][dayIndex];
+}
+
+/* 
+ * Build array of appointments by day
+ */
 const getAppointmentsByDay = (dataArr) => {
     let week = {};
     for(let i = 0; i < 7; i++){
-        week[i] = getRowsByDay(dataArr, i);
+        week[dayOfWeekAsString(i)] = getRowsByDay(dataArr, i);
     }
     return week;
 };
 
+/* 
+ * Get appointments by day of the week
+ */
 const getRowsByDay = (results, day) => {
     return results.filter((row) => {
         const date = convertToDate(row.date);
@@ -48,9 +93,11 @@ const getRowsByDay = (results, day) => {
     });
 };
 
+/* 
+ * Convert the DD/MM/YYYY date from the original CSV into JS date readable
+ * format
+ */
 const convertToDate = (dateString, delim = '/') => {
     const dateParts = dateString.split(delim);
     return new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
 };
-
-

7ce1e37a48216a3da6c6ba741f8807094216576f
Sam Anderson <samel.2710@gmail.com>
2018-01-14 17:45:41 +0000

    Padding the schedule
    Loop through individual day/location schedules and pad empty slots with
    dummy appointments

--- src/js/init.js
+++ src/js/init.js
@@ -1,5 +1,5 @@
 import Papa from 'papaparse';
-import { getDay, setHours, setMinutes } from 'date-fns'
+import { addMinutes, format, getDay, isBefore, isEqual, setHours, setMinutes } from 'date-fns';
 
 const fileInput = document.getElementById('csvFileInput');
 
@@ -35,65 +35,83 @@ const parseCSV = (fileObj) => {
  * Process the parsed results and split into days/tables etc.
  */
 const processData = (results) => {
-    const columns = results.meta.fields;
-
     // Replace date with JS usable start time 
     const processed = results.data.map((meeting) => {
         const {year, month, day} = getDateParts(meeting.date);
         const [startHour, startMinute] = meeting.start_time.split(/\:/);
-        const startTime = new Date(year, month, day, startHour, startMinute);
         
-        meeting.date = startTime;
+        meeting.date = new Date(year, month, day, startHour, startMinute);;
         return meeting;
     });
 
-    const dailyAppointments = getAppointmentsByDay(processed);
-    const dailyAppointmentsByLocation = splitDaysByLocation(dailyAppointments);
+    const week = {};
+    
+    for(let i = 0; i < 7; i++){
+        week[dayOfWeekAsString(i)] = processed.filter(row => getDay(row.date) === i);
+    };
 
-    const paddedAppointments = padAppointments(dailyAppointmentsByLocation);
+    const appointmentsByLocation = {};
+    Object.keys(week).forEach((day) => {
+        if(week[day].length > 0){
+            appointmentsByLocation[day] = splitByTable(week[day]);
+        } else {
+            console.log('No appointments for ', day);
+        }
+    });
 
-    console.log(paddedAppointments);
-};
+    const aptLength = 20;
+    const startHour = 9;
+    const startMinute = 0;
+    const endHour = 18;
+    const endMinute = 0;
 
-/* 
- * Pad a day/locations appointments with breaks to ensure a full 9-6 schedule
- */
- const padAppointments = (appointments, aptLength = 20, startHour = 9, endHour = 18, startMinute = 0, endMinute = 0) => {
-    Object.keys(appointments).forEach((day) => {
-        Object.keys(appointments[day]).forEach((location) => {
-            // Create start/end timestamps for each day/location
-            // const dateStamp = getDateParts(appointments[day][location][0].date);
-            // const startTime = dateStamp.setHours(startHour, startMinute);
-            // const endTime = dateStamp.setHours(endHour, endMinute);
+    Object.keys(appointmentsByLocation).forEach((day) => {
+        const thisDay = day;
 
-            // TO DO: Correctly pad appointments to create full days            
+        Object.keys(appointmentsByLocation[day]).forEach((table) => {
+            const filename = `${day.toLowerCase()}-${table.toLowerCase().replace(/\s+/, '_')}`;
+            const appointments = appointmentsByLocation[day][table];
+            const today = appointments[0].date;
+            let currentTime = setTime(today, startHour, startMinute);
+            const endTime = setTime(today, endHour, endMinute);
+
+            let aptIndex = 0;
+            const paddedAppointments = [];
+
+            while(isBefore(currentTime, endTime)){
+                if(isEqual(appointments[aptIndex].date, currentTime)){
+                    paddedAppointments.push(appointments[aptIndex]);
+                    if(aptIndex < appointments.length - 1){
+                        aptIndex++;
+                    }
+                } else {
+                    paddedAppointments.push(createDummy(currentTime));
+                }
+                currentTime = addMinutes(currentTime, aptLength);
+            }
+            console.log(paddedAppointments);
         });
     });
-    return appointments;
- }
+};
+
+const createDummy = (time) => {
+    return {
+        title: 'Break',
+        date: time,
+        start_time: format(time, 'HH:mm'),
+    };
+};
+
+const setTime = (time, hour, minute) => {
+    return setHours(setMinutes(time, minute), hour);
+};
 
 const sortByStartTime = (arr) => {
     arr.sort((a, b) => {
         return a.start_time.localeCompare(b.start_time);
     });
     return arr;
-}
-
-
-/* 
- * Restructure a days appointments by location
- */
-const splitDaysByLocation = (dailyAppointments) => {
-    let processedAppointments = {};
-    Object.keys(dailyAppointments).forEach((day) => {
-        if(dailyAppointments[day].length > 0){
-            processedAppointments[day] = splitByTable(dailyAppointments[day]);
-        } else {
-            console.log('No appointments for ', day);
-        }
-    });
-    return processedAppointments;
-}
+};
 
 /* 
  * Split a monolothic set of appointments by unique location
@@ -107,24 +125,13 @@ const splitByTable = (appointments) => {
         locations[location] = sortByStartTime(locations[location]);
     });
     return locations;
-}
+};
 
 /* 
  * Helper function to convert day index to human readable
  */
 const dayOfWeekAsString = (dayIndex) => {
     return ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][dayIndex];
-}
-
-/* 
- * Build array of appointments by day
- */
-const getAppointmentsByDay = (dataArr) => {
-    let week = {};
-    for(let i = 0; i < 7; i++){
-        week[dayOfWeekAsString(i)] = dataArr.filter(row => getDay(row.date) === i);
-    }
-    return week;
 };
 
 /* 
@@ -132,7 +139,6 @@ const getAppointmentsByDay = (dataArr) => {
  */
 const getDateParts = (dateString, delim = '/') => {
     const dateParts = dateString.split(delim);
-    console.log(dateString, dateParts);
     return {
         year: dateParts[2],
         month: dateParts[1] - 1,
